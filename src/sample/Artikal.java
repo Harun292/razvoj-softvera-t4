9bc2f00e58417f6fa6cd7e7b9bf3c40e13122c49
Harun <hajkunic1@etf.unsa.ba>
2019-04-01 11:00:36 +0200

    prvi komit

---
+++ src/sample/Artikal.java
@@ -0,0 +1,71 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class Artikal {
+    private String sifra;
+    private String naziv;
+    private double cijena;
+
+    public Artikal() {
+    }
+
+    public Artikal(String recenica){
+        String [] novi=recenica.split(",");
+        sifra=novi[0];
+        naziv=novi[1];
+        cijena=Double.parseDouble(novi[2]);
+    }
+
+    public void setSifra(String sifra) {
+        this.sifra = sifra;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
+
+    public String getSifra() {
+        return sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    @Override
+    public String toString() {
+        return  sifra + "," + naziv + "," + cijena;
+    }
+    public boolean equals(Object o){
+        Artikal artikal=(Artikal)o;
+        if(!naziv.equals(artikal.getNaziv()))
+            return false;
+        if(!sifra.equals(artikal.getSifra()))
+            return false;
+        if(Double.compare(cijena, artikal.getCijena())!=0)
+            return false;
+
+        return true;
+
+    }
+    public static ArrayList<Artikal> izbaciDuplikate(ArrayList<Artikal> artikli){
+        for(int i =0;i<artikli.size();i++){
+            for(int j=i+1;j<artikli.size();j++){
+                if(artikli.get(i).equals(artikli.get(j))){
+                    artikli.remove(j);
+                }
+            }
+        }
+    return artikli;
+    }
+
+}
